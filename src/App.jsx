10fc7364fda522b59334ca3da000e4cb2d4ed1f6
coderarg <lucasgarcia.developer@gmail.com>
2025-09-27 12:01:53 -0300

    Add NotFound page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom'
 import { Inicio } from './routes/Inicio'
 import { AcercaDe } from './routes/AcercaDe'
 import { Organigrama } from './routes/Organigrama'
+import { NotFound } from './routes/404NotFound'
 
 export const App = () => {
 
@@ -23,6 +24,14 @@ export const App = () => {
                     <Route path='/' element={<Inicio />}></Route>
                     <Route path='/acerca de' element={<AcercaDe />}></Route>
                     <Route path='/organigrama' element={<Organigrama />}></Route>
+                    <Route path='/cohercitivo' element={<NotFound />}></Route>
+                    <Route path='/afiliativo' element={<NotFound />}></Route>
+                    <Route path='/participativo' element={<NotFound />}></Route>
+                    <Route path='/delegativo' element={<NotFound />}></Route>
+                    <Route path='/liderazgo conciente' element={<NotFound />}></Route>
+                    <Route path='/liderazgo transformacional' element={<NotFound />}></Route>
+                    <Route path='/7 habitos' element={<NotFound />}></Route>
+                    <Route path='/circulo dorado' element={<NotFound />}></Route>
                 </Routes>
             </main>
         </>
