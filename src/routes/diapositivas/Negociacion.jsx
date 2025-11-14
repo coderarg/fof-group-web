5319e4de4709cf49ccaef0dcaae6645bfbf46d70
coderarg <lucasgarcia.developer@gmail.com>
2025-11-14 18:29:27 -0300

    Corrección de diapositivias

--- src/routes/diapositivas/Negociacion.jsx
+++ src/routes/diapositivas/Negociacion.jsx
@@ -2,8 +2,16 @@ import './Slides.css'
 
 export const Negociacion = () => {
     return (
-        <div className='slide__container'>
-            <img className='slide__img' src="./negociacion-maria-tamara.png" alt="negociacion imagen" />
-        </div>
+        <>
+            <div className='slide__container'>
+                <img className='slide__img' src="./historia_naranjas.png" alt="negociacion imagen" />
+            </div>
+            <div className='slide__container'>
+                <img className='slide__img' src="./humedales.png" alt="negociacion imagen" />
+            </div>
+            <div className='slide__container'>
+                <img className='slide__img' src="./negociacion-maria-tamara.png" alt="negociacion imagen" />
+            </div>
+        </>
     )
 }
